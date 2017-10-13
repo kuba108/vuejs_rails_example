45f84d8bd4c1f1234cb14743dc6db9d2e6dddea6
Kuba Malina <majak108@gmail.com>
2017-10-13 17:07:52 +0200

    Inits Yarn and install Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :employees
+
+  root to: 'employees#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20171013145545_create_employees.rb
+++ db/migrate/20171013145545_create_employees.rb
@@ -1,7 +1,11 @@
 class CreateEmployees < ActiveRecord::Migration[5.1]
   def change
     create_table :employees do |t|
-
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :position, null: false
+      t.string :email, null: false
+      t.string :phone
       t.timestamps
     end
   end
